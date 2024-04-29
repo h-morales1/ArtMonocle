8f1f8c78136c52aab45dd2d8053b1b788c48c2f6
Herbert M. <hmorales5@tutanota.com>
2024-04-28 23:37:12 -0500

    ask for permission to use camera

--- app/src/main/java/h/morales/artmonocle/MainActivity.kt
+++ app/src/main/java/h/morales/artmonocle/MainActivity.kt
@@ -1,8 +1,11 @@
 package h.morales.artmonocle
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,11 +13,35 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
 import h.morales.artmonocle.ui.theme.ArtMonocleTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val cameraPermissionRequest =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                // Implement camera related  code
+            } else {
+                // Camera permission denied (Handle denied operation)
+            }
+
+        }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        when (PackageManager.PERMISSION_GRANTED) {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) -> {
+                // Camera permission already granted
+                // Implement camera
+            }
+            else -> {
+                cameraPermissionRequest.launch(Manifest.permission.CAMERA)
+            }
+        }
+
         setContent {
             ArtMonocleTheme {
                 // A surface container using the 'background' color from the theme
