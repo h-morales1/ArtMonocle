1dbb883751619e8accbbe625a56d316ebb4e00f9
Herbert M. <hmorales5@tutanota.com>
2024-05-19 17:03:32 -0500

    calculator test is red

---
+++ app/src/test/java/h/morales/artmonocle/CalculatorUnitTest.kt
@@ -0,0 +1,23 @@
+package h.morales.artmonocle
+
+import org.junit.Test
+import h.morales.artmonocle.utils.Calculator
+
+import org.junit.Assert.*
+
+class CalculatorUnitTest {
+
+    @Test
+    fun getA_is_correct() {
+        val ab = 9.0f
+        val a = Calculator.getA(ab)
+        assertEquals(a, 5.5624228f)
+    }
+
+    @Test
+    fun getB_is_correct() {
+        val ab = 9.0f
+        val b = Calculator.getB(ab)
+        assertEquals(b, 3.4375772f)
+    }
+}
