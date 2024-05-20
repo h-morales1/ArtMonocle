0e3dab15ddd734d45f5606e58b69e1fb359eb9d0
Herbert M. <hmorales5@tutanota.com>
2024-05-19 22:16:36 -0500

    Line class util

---
+++ app/src/main/java/h/morales/artmonocle/utils/Line.kt
@@ -0,0 +1,17 @@
+package h.morales.artmonocle.utils
+
+class Line {
+    // basically ax,ay => x,y for START
+    private var ax = 0f
+    private var ay = 0f
+
+    private var bx = 0f
+    private var by = 0f
+
+    public fun setAX(yourVal: Float) {
+        ax = yourVal
+    }
+    public fun setAY(yourVal: Float) {}
+    public fun setBX() {}
+    public fun setBY() {}
+}
